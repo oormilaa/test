6e2153c36e14e64f65850f23ec0832bd051f6ea5
afnaanulla <muhmmadafnaan2003@gmail.com>
2024-11-04 18:35:14 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int testcases;
+    cout << "Enter the number of test cases: ";
+    cin >> testcases;
+
+    for (int i = 0; i < testcases; ++i) {
+        int l, r;
+        int n = r - l + 1;
+        if (n ==1)
+        {
+            cout << -1 << endl;
+        }
+        if( l % 2 == 1)
+        {
+            cout << 
+        }
+    }
+
+    return 0;
+}
